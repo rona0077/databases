6d388c42b0415b459d3101a1d393ce741912f510
rona0077 <shukrona.007@mail.ru>
2025-12-14 20:59:04 +0500

    Add files via upload

---
+++ практика6/code.sql
@@ -0,0 +1,46 @@
+CREATE TABLE Students (
+    student_id SERIAL PRIMARY KEY, -- Уникальный идентификатор студента
+    full_name VARCHAR(255) NOT NULL, -- Полное имя студента
+    email VARCHAR(255) UNIQUE NOT NULL, -- Электронная почта студента (должна быть уникальной)
+    start_year INT -- Год поступления студента
+);
+
+CREATE TABLE Courses (
+    course_id SERIAL PRIMARY KEY, -- Уникальный идентификатор курса
+    course_name VARCHAR(255) NOT NULL, -- Название курса
+    credits INT CHECK (credits > 0) -- Количество зачетных единиц курса (должно быть больше 0)
+);
+
+CREATE TABLE Enrollments (
+    student_id INT REFERENCES Students(student_id) ON DELETE CASCADE, -- Ссылка на ID студента (внешний ключ)
+    course_id INT REFERENCES Courses(course_id) ON DELETE CASCADE, -- Ссылка на ID курса (внешний ключ)
+    grade CHAR(1), -- Оценка студента за курс (например, 'A', 'B', 'C')
+    PRIMARY KEY (student_id, course_id)
+);
+INSERT INTO Students (full_name, email, start_year) VALUES
+('Алексей Смирнов','smirnov@mail.ru', 2021),
+('Елена Кузнецова','elena@mail.ru', 2022),
+('Дмитрий Новиков','novikov@mail.ru', 2021),
+('Ольгу Морозову','moroz@mail.ru', 2023);
+
+INSERT INTO Courses (course_name, credits) VALUES
+('Введение в программирование',5),
+('База данных',4),
+('Веб-технологии',4);
+INSERT INTO enrollments(student_id,course_id,grade)VALUES
+(7,2,'A'),
+(8,2,'A'),
+(9,1,NULL),
+(9,2,NULL),
+(9,3,NULL);
+UPDATE students
+SET email='elena.kuznetsova@newmail.com'
+WHERE email='elena@mail.ru';
+UPDATE enrollments 
+SET grade='A'
+WHERE course_id = 1 and student_id = 9;
+DELETE FROM Students
+WHERE email = 'moroz@mail.ru';
+
+
+
